924de4d4d809e29c2a74e50f1206638d5e96c86b
Bruno Nunes <bruno.nunes72@hotmail.com>
2025-05-15 19:09:03 -0300

    correcao

--- src/services/N8NService.ts
+++ src/services/N8NService.ts
@@ -2,20 +2,18 @@ import axios from "axios";
 
 class N8NService {
   private static url = process.env.N8N_WEBHOOK_URL;
-  private static url_test = process.env.N8N_WEBHOOK_UR_TEST;
+  private static url_test = process.env.N8N_WEBHOOK_URL_TEST;
 
   public static async sendWebhook(data: any) {
-    console.log("Enviando webhook para N8N:", this.url);
-
-    const response = await axios.post(this.url!, data);
-    if (response.status !== 200) {
-      console.error("Erro ao enviar webhook:", response.statusText);
+    try {
+      const response = await axios.post(this.url!, data);
+      console.log("Webhook enviado com sucesso:", response.data);
+    } catch (error) {
+      console.error("Erro ao enviar webhook:", error);
       await this.sendWebhookTest(data);
     }
   }
   private static async sendWebhookTest(data: any) {
-    console.log("Enviando webhook de TESTE para N8N:", this.url_test);
-    
     try {
       const response = await axios.post(this.url_test!, data);
       console.log("Webhook enviado com sucesso:", response.data);
